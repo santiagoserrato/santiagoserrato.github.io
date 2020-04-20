f58a15357ab35a51d1397f167e90b0d9f46b701a
santiagoserrato <santiagoserrato@gmail.com>
2020-04-20 05:57:21 -0500

    Updates

---
+++ precache-manifest.c2cb8cb0e7ddfa31c4a48e511b96de57.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e729f8370abbf81f2439678ec53404e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3213b5f5c82209434aee",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3f9e67ea55ab5b88e54f",
+    "url": "/static/css/main.ebd43d67.chunk.css"
+  },
+  {
+    "revision": "3213b5f5c82209434aee",
+    "url": "/static/js/2.a86ded75.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a86ded75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9e67ea55ab5b88e54f",
+    "url": "/static/js/main.679a4f13.chunk.js"
+  },
+  {
+    "revision": "89249adbda5416db5ccb",
+    "url": "/static/js/runtime-main.6a36e423.js"
+  },
+  {
+    "revision": "4082623ae3f18cbd25c66c4045f02edf",
+    "url": "/static/media/emailIcon.4082623a.svg"
+  },
+  {
+    "revision": "647be3b71f222752e2589593eaad6a08",
+    "url": "/static/media/gitHubIcon.647be3b7.svg"
+  },
+  {
+    "revision": "8cc2d3a80c1ebec192c2f29c08333dc6",
+    "url": "/static/media/linkedInIcon.8cc2d3a8.svg"
+  },
+  {
+    "revision": "481b4bf6aac2aac50738d4f2ae38c87a",
+    "url": "/static/media/logo.481b4bf6.svg"
+  },
+  {
+    "revision": "00ef514a0275722bcdf956f5c28bf252",
+    "url": "/static/media/santiago.00ef514a.jpg"
+  },
+  {
+    "revision": "d20a0344ed7c51161d2090c3fe6461e0",
+    "url": "/static/media/streamLogo.d20a0344.png"
+  },
+  {
+    "revision": "e9fa644067132a3e17a32c4fdbeb32d2",
+    "url": "/static/media/untLogo.e9fa6440.png"
+  },
+  {
+    "revision": "f2b5132f256d33e015995fd1eeb252c8",
+    "url": "/static/media/untOfficeOfSustainabilityLogo.f2b5132f.svg"
+  }
+]);
