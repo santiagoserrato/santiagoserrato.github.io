aeddbb4169228b74b7b78442cfff94f4c00579ad
Santiago Serrato <santiagoserrato@gmail.com>
2021-04-08 00:56:18 -0500

    Convert to Typescript

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FunctionComponent = () => {
+    return (
+        <div className="text-light text-center py-3" style={{backgroundImage: "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)"}}>
+            Copyright © 2020 Santiago Serrato. All Rights Reserved.
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import {ReactComponent as Logo} from './../assets/images/logo.svg';
+
+import {ReactComponent as LinkedInIcon} from './../assets/images/linkedInIcon.svg';
+import {ReactComponent as GitHubIcon} from './../assets/images/gitHubIcon.svg';
+import {ReactComponent as EmailIcon} from './../assets/images/emailIcon.svg';
+
+const Navigation: React.FunctionComponent = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+        <Logo fill='#333' width='3rem'height='3rem'/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#aboutMe">About Me</Nav.Link>
+            <Nav.Link href="#experience">Work Experience</Nav.Link>
+            <Nav.Link href="#education">Education</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+          </Nav>
+          <Navbar.Text className="d-none d-lg-block">
+            <a href="https://www.linkedin.com/in/santiagoserrato/"target="__blank"><LinkedInIcon className="px-1" fill='#333' width='3rem'height='2rem' /></a>
+            <a href="https://github.com/santiagoserrato"target="__blank"><GitHubIcon className="px-1" fill='#333' width='3rem'height='2rem' /></a>
+            <a href="mailto:santiagoserrato@gmail.com"target="__blank"><EmailIcon className="px-1" fill='#333' width='3rem'height='2rem' /></a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
