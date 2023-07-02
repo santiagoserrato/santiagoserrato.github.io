6a7d51d579fd177653e1136a8e7299f53bac0586
Santiago Serrato <1375961+santiagoserrato@users.noreply.github.com>
2023-07-02 16:51:41 -0500

    Display current year in footer copyright message

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Footer: React.FunctionComponent = () => {
+    const currentYear = new Date().getFullYear();
+    
     return (
         <div className="text-light text-center py-3" style={{backgroundImage: "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)"}}>
-            Copyright © 2020 Santiago Serrato. All Rights Reserved.
+            Copyright © {currentYear} Santiago Serrato. All Rights Reserved.
         </div>
     );
 }
